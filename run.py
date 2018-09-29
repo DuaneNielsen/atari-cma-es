691bb8b1b1a90e21fb13628e0b56da8d4defcaf2
niedu02 <duane.nielsen.rocks@gmail.com>
2018-09-28 20:21:11 -0700

    Added Atari encoder

--- run.py
+++ run.py
@@ -2,7 +2,10 @@ import gym
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
+from mentalitystorm import config, Storeable
 
+device = config.device()
+visuals = Storeable.load('C:\data\models\GM53H301W5YS38XH').to(device)
 
 def top_25_percent(scores, higher_is_better=True):
     """
@@ -17,16 +20,16 @@ def top_25_percent(scores, higher_is_better=True):
     return torch.tensor(best), torch.tensor(rest)
 
 
-env = gym.make('CartPole-v1')
+env = gym.make('SpaceInvaders-v4')
 policy_nets = []
 weights = []
 scores = []
-sample_size = 40
-epochs = 20
-rollouts = 1
+sample_size = 50
+epochs = 10
+rollouts = 3
 
 for net in range(sample_size):
-    policy_nets.append(nn.Sequential(nn.Linear(4, 1), nn.Sigmoid()).double())
+    policy_nets.append(nn.Sequential(nn.Linear(16, 6), nn.Softmax(dim=1)).double())
     scores.append(0.0)
     weight = policy_nets[net]._modules['0'].weight.data
     weights.append(weight)
@@ -38,18 +41,22 @@ sigma = torch.sqrt(weights_t.var(0))
 for epoch in range(epochs):
     for net in range(sample_size):
         distrib = Normal(mu, sigma)
-        weights[net] = distrib.sample((1,)).view((1,4))
+        weights[net] = distrib.sample((1,)).view((6, 16))
         policy_nets[net]._modules['0'].weight.data = weights[net].data
 
     for i, net in enumerate(policy_nets):
         for i_episode in range(rollouts):
-            raw_observation = env.reset()
+            env.reset()
             for t in range(100):
-                obs = torch.tensor(raw_observation)
-                action = net(obs)
-                action = int(round(action.item()))
+                screen = env.render(mode='rgb_array')
+                obs = torch.tensor(screen).cuda().float()
+                obs = obs.permute(2, 0, 1).unsqueeze(0)
+                latent, sigma = visuals.encoder(obs)
+                latent = latent.cpu().double().squeeze(3).squeeze(2)
+                action = net(latent)
+                _, the_action = action.max(1)
                 env.render()
-                raw_observation, reward, done, info = env.step(action)
+                raw_observation, reward, done, info = env.step(the_action.item())
                 scores[i] += reward
                 if done:
                     print("Episode finished after {} timesteps".format(t+1))
