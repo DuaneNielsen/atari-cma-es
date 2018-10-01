38dc8aad683c2340771b3afd3eaed0e2b76b5311
Duane <duane.nielsen.rocks@gmail.com>
2018-09-30 17:48:33 -0700

    Fixed Space Invaders Hopefully it works now!

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
 from mentalitystorm import config, Storeable
+import numpy as np
 
 device = config.device()
-visuals = Storeable.load('C:\data\models\GM53H301W5YS38XH').to(device)
+visuals = Storeable.load('D:\data\models\GM53H301W5YS38XH').to(device)
 
 def top_25_percent(scores, higher_is_better=True):
     """
@@ -24,9 +25,9 @@ env = gym.make('SpaceInvaders-v4')
 policy_nets = []
 weights = []
 scores = []
-sample_size = 50
-epochs = 10
-rollouts = 3
+sample_size = 200
+epochs = 200
+rollouts = 1
 
 for net in range(sample_size):
     policy_nets.append(nn.Sequential(nn.Linear(16, 6), nn.Softmax(dim=1)).double())
@@ -47,7 +48,7 @@ for epoch in range(epochs):
     for i, net in enumerate(policy_nets):
         for i_episode in range(rollouts):
             env.reset()
-            for t in range(100):
+            for t in range(5000):
                 screen = env.render(mode='rgb_array')
                 obs = torch.tensor(screen).cuda().float()
                 obs = obs.permute(2, 0, 1).unsqueeze(0)
@@ -59,16 +60,20 @@ for epoch in range(epochs):
                 raw_observation, reward, done, info = env.step(the_action.item())
                 scores[i] += reward
                 if done:
-                    print("Episode finished after {} timesteps".format(t+1))
+                    #print("Episode finished after {} timesteps".format(t+1))
                     break
 
-    print(scores)
+    scores_np = np.array(scores)
+    score_mean = scores_np.mean()
+    score_var = scores_np.var()
     best, rest = top_25_percent(scores)
+    best_score = scores[best[0].item()]
     for i, elem in enumerate(scores):
         scores[i] = 0.0
     weights_t = torch.stack(weights)
     mu = weights_t[best].mean(0)
     sigma = torch.sqrt(weights_t.var(0))
+    print('SCORE: mean %f, variance %f, best %f, CME: mean %f, sigma %f' % (score_mean, score_var, best_score, mu.view(6*16).mean(0), sigma.view(6*16).mean()))
     with open('best_model', 'wb') as f:
         torch.save(policy_nets[best[0].item()], f)
 
