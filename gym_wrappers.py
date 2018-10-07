ba93623c1fc7c06394683ccb9a69b18e2d1428e5
Duane <duane.nielsen.rocks@gmail.com>
2018-10-07 16:41:25 -0700

    Added reward wrapper

---
+++ gym_wrappers.py
@@ -0,0 +1,32 @@
+from gym import Wrapper
+
+class StepReward(Wrapper):
+    def __init__(self, env, step_reward=0):
+        super(StepReward, self).__init__(env)
+        self.step_reward = step_reward
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+
+        reward = reward + self.step_reward
+
+        return observation, reward, done, info
+
+    def reset(self):
+        return self.env.reset()
+
+
+class StepOnlyReward(Wrapper):
+    def __init__(self, env, step_reward=0):
+        super(StepOnlyReward, self).__init__(env)
+        self.step_reward = step_reward
+
+    def step(self, action):
+        observation, reward, done, info = self.env.step(action)
+
+        reward = self.step_reward
+
+        return observation, reward, done, info
+
+    def reset(self):
+        return self.env.reset()
