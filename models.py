1610e3b926bacd30eb9d6b8aefa9acf21a98b187
niedu02 <duane.nielsen.rocks@gmail.com>
2018-10-06 20:40:02 -0700

    Refactored to CMA-ES handle any nn.Module PolicyNet has a convolution

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from mentalitystorm import Storeable, BaseVAE
+from mentalitystorm.basemodels import BaseAE
+from mentalitystorm.util import conv_output_shape
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 
 """
 input_shape is a tuple of (height,width)
@@ -64,6 +65,103 @@ class ConvVAE4Fixed(Storeable, BaseVAE):
             self.d_bn3 = nn.BatchNorm2d(32)
 
             self.d_conv4 = nn.ConvTranspose2d(32, 3, kernel_size=first_kernel, stride=first_stride, output_padding=1)
+
+        def forward(self, z):
+            decoded = F.relu(self.d_bn1(self.d_conv1(z)))
+            decoded = F.relu(self.d_bn2(self.d_conv2(decoded)))
+            decoded = F.relu(self.d_bn3(self.d_conv3(decoded)))
+            decoded = self.d_conv4(decoded)
+            return torch.sigmoid(decoded)
+
+
+class PolicyNet(nn.Module):
+    def __init__(self, input_size, actions):
+        nn.Module.__init__(self)
+        self.num_actions = actions
+
+        self.conv = nn.Conv2d(2, 1, kernel_size=2, stride=2)
+        output_shape = conv_output_shape(input_size, kernel_size=2, stride=2)
+        self.output_len = output_shape[0] * output_shape[1]
+
+        self.lin = nn.Linear(self.output_len, actions)
+        self.action = nn.Softmax(dim=1)
+
+    def forward(self, z):
+        action = F.leaky_relu(self.conv(z))
+        action = self.lin(action.view(-1, self.output_len))
+        return self.action(action)
+
+
+class Compressor(Storeable, BaseAE):
+    """
+    input_shape is a tuple of (height, width)
+    """
+    def __init__(self, input_shape, z_size, input_channels=3, output_channels=3, first_kernel=5,
+                 first_stride=2, second_kernel=5, second_stride=2):
+        self.input_shape = input_shape
+        self.z_size = z_size
+        encoder = self.Encoder(input_shape, z_size, input_channels, first_kernel, first_stride, second_kernel, second_stride)
+        decoder = self.Decoder(z_size, encoder.z_shape, output_channels, first_kernel, first_stride, second_kernel, second_stride)
+        BaseAE.__init__(self, encoder, decoder)
+        Storeable.__init__(self)
+
+    class Encoder(nn.Module):
+        def __init__(self, input_shape, z_size, channels=3, first_kernel=5, first_stride=2, second_kernel=5, second_stride=2):
+            nn.Module.__init__(self)
+            # batchnorm in autoencoding is a thing
+            # https://arxiv.org/pdf/1602.02282.pdf
+
+            from mentalitystorm.util import conv_output_shape
+
+
+            s1 = 32
+            s2 = 64
+            # encoder
+            self.e_conv1 = nn.Conv2d(channels, s1, kernel_size=first_kernel, stride=first_stride)
+            self.e_bn1 = nn.BatchNorm2d(s1)
+            output_shape = conv_output_shape(input_shape, kernel_size=first_kernel, stride=first_stride)
+
+            self.e_conv2 = nn.Conv2d(s1, s2, kernel_size=second_kernel, stride=second_stride)
+            self.e_bn2 = nn.BatchNorm2d(s2)
+            output_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
+
+            self.e_conv3 = nn.Conv2d(s2, s2, kernel_size=second_kernel, stride=second_stride)
+            self.e_bn3 = nn.BatchNorm2d(s2)
+            output_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
+
+            self.e_conv4 = nn.Conv2d(s2, z_size, kernel_size=2, stride=2)
+            self.e_bn4 = nn.BatchNorm2d(z_size)
+            self.z_shape = conv_output_shape(output_shape, kernel_size=2, stride=2)
+
+        def forward(self, x):
+            encoded = F.relu(self.e_bn1(self.e_conv1(x)))
+            encoded = F.relu(self.e_bn2(self.e_conv2(encoded)))
+            encoded = F.relu(self.e_bn3(self.e_conv3(encoded)))
+            encoded = F.relu(self.e_bn4(self.e_conv4(encoded)))
+            return encoded
+
+    class Decoder(nn.Module):
+        def __init__(self, z_size, z_shape, channels=3, first_kernel=5, first_stride=2, second_kernel=5, second_stride=2):
+            nn.Module.__init__(self)
+
+            s1 = 32
+            s2 = 64
+
+            self.d_conv1 = nn.ConvTranspose2d(z_size, s2, kernel_size=2, stride=2, output_padding=1)
+            self.d_bn1 = nn.BatchNorm2d(s2)
+            # decoder
+            #self.d_conv1 = nn.ConvTranspose2d(z_size, 128, kernel_size=z_shape, stride=1)
+            #self.d_bn1 = nn.BatchNorm2d(128)
+
+            self.d_conv2 = nn.ConvTranspose2d(s2, s2, kernel_size=second_kernel, stride=second_stride,
+                                              output_padding=(1, 0))
+            self.d_bn2 = nn.BatchNorm2d(s2)
+
+            self.d_conv3 = nn.ConvTranspose2d(s2, s1, kernel_size=second_kernel, stride=second_stride,
+                                              output_padding=(0, 1))  # output_padding=1)
+            self.d_bn3 = nn.BatchNorm2d(s1)
+
+            self.d_conv4 = nn.ConvTranspose2d(s1, channels, kernel_size=first_kernel, stride=first_stride, output_padding=1)
 
         def forward(self, z):
             decoded = F.relu(self.d_bn1(self.d_conv1(z)))
--- run.py
+++ run.py
@@ -2,18 +2,32 @@ import gym
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
-from mentalitystorm import config, Storeable, OpenCV
+from mentalitystorm import config, Storeable, Run, ImageViewer
+from mentalitystorm.basemodels import MultiChannelAE
+import torchvision.transforms as TVT
+import mentalitystorm.transforms as tf
 import numpy as np
+from models import PolicyNet
+from pathlib import Path
+from collections import namedtuple
 
 device = config.device()
 #visuals = Storeable.load('D:\data\models\GM53H301W5YS38XH').to(device)
-visuals = Storeable.load(r'.\modelzoo\run2\goodvisual.pik').to(device)
-view_latent = OpenCV('latent', (320, 480))
+shot_encoder = Run.load_model(r'c:\data\runs\549\shots_v1\epoch0060.run').to(device=config.device())
+player_encoder = Run.load_model(r'c:\data\runs\580\shots_v1\epoch0081.run').to(device=config.device())
+visuals = MultiChannelAE()
+visuals.add_ae(shot_encoder, [0, 2, 3])
+visuals.add_ae(player_encoder, [1, 2, 3])
+
+transforms = TVT.Compose([tf.CoordConv()])
+
+view_latent = ImageViewer('latent', (320, 480))
+
 
 def view_image(model, input, output):
     view_latent.update(output[0].data)
 
-visuals.decoder.register_forward_hook(view_image)
+#visuals.decoder.register_forward_hook(view_image)
 
 def top_25_percent(scores, higher_is_better=True):
     """
@@ -23,15 +37,101 @@ def top_25_percent(scores, higher_is_better=True):
     """
     indexed = [(i, s) for i, s in enumerate(scores)]
     indexed = sorted(indexed, key=lambda score: score[1], reverse=higher_is_better)
-    best = [indexed[i][0] for i in range(len(indexed)//4)]
-    rest = [indexed[i][0] for i in range(len(indexed)//4+1, len(indexed))]
+    num_winners = len(indexed) // 4
+    num_winners = num_winners if num_winners > 0 else 1
+    best = [indexed[i][0] for i in range(num_winners)]
+    rest = [indexed[i][0] for i in range(num_winners+1, len(indexed))]
     return torch.tensor(best), torch.tensor(rest)
+
+
+def flatten(net):
+    w = []
+
+    def _capture(m):
+        if type(m) in [nn.Linear, nn.Conv2d]:
+            w.append(m.weight.data)
+            w.append(m.bias.data)
+
+    net.apply(_capture)
+
+    t = list(map(lambda x: x.view(-1), w))
+    return torch.cat(t)
+
+
+def restore(net, t):
+    start = 0
+
+    def _restore(m):
+        if type(m) in [nn.Linear, nn.Conv2d]:
+            nonlocal start
+
+            length = m.weight.data.numel()
+            chunk = t[range(start, start + length)]
+            m.weight.data = chunk.view(m.weight.data.shape)
+            start += length
+
+            length = m.bias.data.numel()
+            chunk = t[range(start, start + length)]
+            m.bias.data = chunk.view(m.bias.data.shape)
+            start += length
+
+    net.apply(_restore)
+
+
+class CMA:
+    def __init__(self, higher_is_better=True):
+
+        self.weights = []
+        self.scores = []
+        self.higher_is_better = higher_is_better
+        self.stats = []
+        self.distrib = None
+
+    def add(self, net, score, stats=None):
+        self.weights.append(flatten(net))
+        self.scores.append(score)
+        if stats is not None:
+            self.stats.append(stats)
+
+    def rank_and_compute(self):
+        w_t = torch.stack(self.weights)
+        best, rest = top_25_percent(self.scores, self.higher_is_better)
+        mu = w_t[best].mean(0)
+        stdv = torch.sqrt(w_t.var(0))
+        self.distrib = Normal(mu, stdv)
+
+        self.print_scores()
+
+        self.weights = []
+        self.scores = []
+        self.stats = []
+        return best, rest
+
+    def set_sample_weights(self, net):
+        sample = self.distrib.sample((1,)).squeeze(0)
+        restore(net, sample)
+
+    def print_scores(self):
+        if self.scores is not None:
+            best, rest = top_25_percent(self.scores, self.higher_is_better)
+            scores_np = np.array(self.scores)
+            score_mean = scores_np.mean()
+            score_var = scores_np.var()
+            best_score = self.scores[best[0].item()]
+            episode_steps = [d['episode_steps'] for d in self.stats]
+            episode_steps_np = np.array(episode_steps)
+            print('SCORE: mean %f, variance %f, best %f, ' \
+                   'epi mean length %f, epi max len %f, ' \
+                   'CME: mean %f, sigma %f' % (
+                       score_mean, score_var, best_score,
+                       episode_steps_np.mean(), episode_steps_np.max(),
+                       self.distrib.mean.mean(), self.distrib.stddev.mean()))
 
 
 env = gym.make('SpaceInvaders-v4')
 policy_nets = []
-weights = []
-scores = []
+cma = CMA()
+
 episode_steps = []
 sample_size = 300
 epochs = 200
@@ -39,62 +139,59 @@ rollouts = 1
 
 z_size = 32
 
-for net in range(sample_size):
-    policy_nets.append(nn.Sequential(nn.Linear(z_size, 6), nn.Softmax(dim=1)).double())
-    scores.append(0.0)
-    episode_steps.append(0)
-    weight = policy_nets[net]._modules['0'].weight.data
-    weights.append(weight)
 
-weights_t = torch.stack(weights)
-mu = weights_t.mean(0)
-sigma = torch.sqrt(weights_t.var(0))
+for net in range(sample_size):
+    policy_nets.append(PolicyNet((11, 8), 6).double())
+    #policy_nets.append(nn.Sequential(nn.Linear(4, 2), nn.Softmax(dim=1)))
+    episode_steps.append(0)
+
 
 for epoch in range(epochs):
-    for net in range(sample_size):
-        distrib = Normal(mu, sigma)
-        weights[net] = distrib.sample((1,)).view((6, z_size))
-        policy_nets[net]._modules['0'].weight.data = weights[net].data
-
-    for i, net in enumerate(policy_nets):
+    for net in policy_nets:
+        score = 0
+        stats = {'episode_steps': 0}
         for i_episode in range(rollouts):
-            env.reset()
+            raw_observation = env.reset()
             for t in range(5000):
                 screen = env.render(mode='rgb_array')
                 obs = torch.tensor(screen).cuda().float()
-                obs = obs.permute(2, 0, 1).unsqueeze(0)
-                latent, sigma = visuals.encoder(obs)
-                visuals.decoder(latent)
+                obs = obs.permute(2, 0, 1)
+                obs = transforms(obs)
+                obs = obs.unsqueeze(0)
+                latent = visuals(obs)
+                #todo implement decoding
+                #visuals.decoder(latent)
                 latent = latent.cpu().double().squeeze(3).squeeze(2)
                 action = net(latent)
+                #action = net(torch.tensor(raw_observation).float().unsqueeze(0))
                 _, the_action = action.max(1)
                 env.render()
                 raw_observation, reward, done, info = env.step(the_action.item())
-                scores[i] += reward
-                episode_steps[i] += 1
+                score += reward
+                stats['episode_steps'] += 1
                 if done:
                     break
 
-    scores_np = np.array(scores)
-    score_mean = scores_np.mean()
-    score_var = scores_np.var()
-    best, rest = top_25_percent(scores)
-    best_score = scores[best[0].item()]
-    episode_steps_np = np.array(episode_steps)
-    for i, elem in enumerate(scores):
-        scores[i] = 0.0
-        episode_steps[i] = 0
-    weights_t = torch.stack(weights)
-    mu = weights_t[best].mean(0)
-    sigma = torch.sqrt(weights_t.var(0))
-    print('SCORE: mean %f, variance %f, best %f, epi mean length %f, epi max len %f, CME: mean %f, sigma %f' % (score_mean, score_var, best_score, episode_steps_np.mean(), episode_steps_np.max(), mu.view(6*z_size).mean(0), sigma.view(6*z_size).mean()))
-    filename = 'modelzoo/run2' + 'best_model%d'%epoch
-    with open(filename, 'wb') as f:
+        cma.add(net, score, stats)
+
+    best, rest = cma.rank_and_compute()
+
+    save_path = Path('modelzoo/run2')
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    filename = save_path / ('best_model%d' % epoch)
+    with filename.open('wb') as f:
         torch.save(policy_nets[best[0].item()], f)
-    filename = 'modelzoo/' + 'musigma_%d' % epoch
-    with open(filename, 'wb') as f:
+
+    filename = save_path / ('cma_%d' % epoch)
+    with filename.open('wb') as f:
         import pickle
-        pickle.dump((mu.numpy(), sigma.numpy()), f)
+        pickle.dump(cma, f)
+
+    for net in policy_nets:
+        cma.set_sample_weights(net)
+
+
 
 
 
