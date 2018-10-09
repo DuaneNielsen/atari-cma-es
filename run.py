66bb70f025c396a44e9849acf946fdb5a1497f40
niedu02 <duane.nielsen.rocks@gmail.com>
2018-10-09 15:32:35 -0700

    Resuming works

--- run.py
+++ run.py
@@ -64,7 +64,7 @@ env = gym.make('SpaceInvaders-v4')
 env = gym_wrappers.StepReward(env, step_reward=1)
 
 policy_nets = []
-cma_file = r'C:\data\SpaceInvaders-v4\policy_runs\602\cma_7'
+cma_file = r'C:\data\SpaceInvaders-v4\policy_runs\603\cma_8'
 
 with Path(cma_file).open('rb') as f:
     cma = pickle.load(f)
